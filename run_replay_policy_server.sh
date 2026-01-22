ac696bf574a3fc555d7fa9674c4ecc4dcf3c8a81
Haoxu Huang <huanghx24@mails.tsinghua.edu.cn>
2026-01-22 18:53:32 +0800

    Add replay policy server, fix timestamp loading.

---
+++ run_replay_policy_server.sh
@@ -0,0 +1,6 @@
+python -m lerobot.scripts.lbm_replay_policy_server \
+    --repo-id lbm-eval-BimanualPlaceAppleFromBowlIntoBin \
+    --dataset-root /cephfs/huanghaoxu/Data/LBM_lerobot_dataset/lbm-eval-BimanualPlaceAppleFromBowlIntoBin \
+    --episodes 0 \
+    --server-uri localhost:50051
+
